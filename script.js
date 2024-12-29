5235cf21618ef2e5567496c75c6d7acd6631fc0f
Bala Santhosh B <147497426+unstopablethinkerr@users.noreply.github.com>
2024-12-29 10:09:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (dateText) {
             const examDate = new Date(dateText[0].split('-').reverse().join('-'));
             const today = new Date();
+            const tomorrow = new Date(today);
+            tomorrow.setDate(today.getDate() + 1);
+            
 
-            if (examDate < today) {
+            if (examDate < tomorrow) {
                 header.style.background = 'linear-gradient(to right, #ff0000, #ff5555)';
                 header.style.color = '#ffffff';
                 header.style.cursor = 'not-allowed';
